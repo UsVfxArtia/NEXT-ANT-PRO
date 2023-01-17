27148fd38365b296071c57e383616a9d88042d87
UsVfxArtia <112181162+UsVfxArtia@users.noreply.github.com>
2023-01-17 22:35:14 +0300

    Updated Codebase

---
+++ components/TheContainer.tsx
@@ -0,0 +1,13 @@
+import { ProForm, ProFormText } from "@ant-design/pro-components";
+
+export default () => {
+  return (
+    <ProForm
+      onFinish={async (values) => {
+        console.log(values);
+      }}
+    >
+      <ProFormText name="name" label="姓名" />
+    </ProForm>
+  );
+};
---
+++ pages/about/index.tsx
@@ -0,0 +1,5 @@
+import TheContainer from "../../components/TheContainer";
+
+export default function about() {
+  return <TheContainer />;
+}
